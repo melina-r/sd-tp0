025df85079aef9bf7207a213279bb0f9d33daf38
melina <mretamozo@fi.uba.ar>
2026-08-26 16:52:28 -0300

    feat: ej4 - short write/read

--- services/client/src/safe_socket/safe_socket.go
+++ services/client/src/safe_socket/safe_socket.go
@@ -5,18 +5,30 @@ import "io"
 //TODO: Complete with a short-read/short-write tolerant implementation
 
 func SendAll(socket io.Writer, bytes []byte) error {
-	_, err := socket.Write(bytes)
+	bytes_sent, err := socket.Write(bytes)
+
+	for (bytes_sent < len(bytes) && err == nil) {
+		bytes_sent, err = socket.Write(bytes[bytes_sent:])
+		
+	}
+
 	if err != nil {
 		return err
-	}
+	}	
+
 	return nil
 }
 
 func RecvAll(socket io.Reader, size int) ([]byte, error) {
 	buff := make([]byte, size)
-	n, err := socket.Read(buff)
+	bytes_read, err := socket.Read(buff)
+
+	for (bytes_read < size && err == nil) {
+		bytes_read, err = socket.Read(buff[bytes_read:])
+	}
+
 	if err != nil {
 		return nil, err
 	}
-	return buff[:n], nil
+	return buff, nil
 }
